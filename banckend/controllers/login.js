ac0d2a6e1c7a2d923f564a533258bbdfef27ea62
Dddxx98 <deexith2016@gmail.com>
2024-12-20 14:20:13 +0530

    loginVerify

--- banckend/controllers/login.js
+++ banckend/controllers/login.js
@@ -7,9 +7,9 @@ exports.loginUser = (req, res, next) => {
         if (!result) {
             return res.status(404).json({ message: "User not found" });
         } else if(result.password !== password) {
-            return res.status(401).json({ message: "Invalid password" });
+            return res.status(401).json({ message: "User not authorized" });
         }
-        return res.status(200).json({ result });
+        return res.status(200).json({message:"User login sucessful", result });
     }).catch(err => {
         return res.status(500).json(err.original.code);
     });
